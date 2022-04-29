5e6fadd4f1a686b9b527b5e3f740f67d5e27ee1b
MASAYORI <o.massa1117@gmail.com>
2022-04-29 23:10:48 +0900

    少数をできる限り防ぐ

---
+++ atcoder_189B.py
@@ -0,0 +1,17 @@
+
+def main():
+    N, X = map(int, input().split())
+    drink = 0
+    i = 0
+    for i in range(N):
+        V, P = map(int, input().split())
+        drink += V * P
+        if drink > X*100:
+            print(i+1)
+            exit()
+
+    print(-1)
+
+
+if __name__ == '__main__':
+    main()
